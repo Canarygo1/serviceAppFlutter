6c23fe113de452663df1a3ca59bb432dcc405468
Alejandro <Canarygo1@gmail.com>
2020-02-20 20:23:34 +0100

    login finished!

---
+++ lib/login_ui/login_presenter.dart
@@ -0,0 +1,25 @@
+import 'package:serviceapp/data/repository/remote/remote_repository.dart';
+import 'package:serviceapp/model/jsonPostLogin.dart';
+
+class LoginPresenter {
+  LoginView _view;
+  RemoteRepository remoteRepository;
+
+  LoginPresenter(this._view, this.remoteRepository);
+
+  onLoginClicked(String username, String password) async {
+    try {
+      JsonPostLogin jsonPostLogin = JsonPostLogin(username, password);
+      await remoteRepository.postLogin(jsonPostLogin);
+      _view.loginCorrect(false);
+    } on Exception catch (e) {
+      _view.loginError();
+    }
+  }
+}
+
+abstract class LoginView {
+  loginCorrect(bool repuesta) {}
+
+  loginError() {}
+}
